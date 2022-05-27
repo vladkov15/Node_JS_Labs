c4b0b6165d86d85dbd8c8eedcc3cc1c8a7fe55b3
vladislav_kovaliov <vkov2001@gmai.com>
2022-05-27 17:17:05 +0300

    add Lab4

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from "morgan";
 import clientsRouter from './resources/client/client.router';
 import productsRouter from './resources/products/products.router';
 import ordersRouter from './resources/orders/orders.router';
+import { logger, errorHandler} from "./middleware";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,6 +17,14 @@ app.use('/', (req, res, next) => {
   }
   next();
 });
+
+app.use(morgan('[:date[Europe/Minsk]] :method :url :status :response-time ms :body', {
+  stream: { write: message => logger.http(message) }
+}));
+app.use(errorHandler);
+
+process.on('Опа Ошибкочка!', (error: Error) => logger.error(error));
+process.on('Опять ошика!', (error: Error) => logger.error(error));
 
 app.use('/clients', clientsRouter);
 app.use('/products', productsRouter);
